44983c4dc5945c05b3ad421ac1465f04e2116adc
Dale Wijnand <dale.wijnand@gmail.com>
2016-07-01 00:20:19 +0100

    Embed much much more in quasiquotes, nicer

--- src/main/scala/dataclasses/data.scala
+++ src/main/scala/dataclasses/data.scala
@@ -7,91 +7,80 @@ import scala.runtime.ScalaRunTime
 
 class data extends scala.annotation.StaticAnnotation {
   inline def apply(defn: Any) = meta {
-    val q"..$mods class $tname[..$tparams] ..$ctorMods (...$paramss) extends $template" = defn
-    val template"{ ..$earlydefns } with ..$ctorcalls { $param => ..$stats }" = template
+    val q"""
+      ..$mods0 class $tname[..$tparams] ..$ctorMods (...$paramss0) extends { ..$earlyStats } with ..$ctorcalls {
+        $selfParam =>
+        ..$stats
+      }
+    """ = defn
 
-    val tnameString  = tname.value
-    val ScalaRunTime = q"_root_.scala.runtime.ScalaRunTime"
-    val Any          = t"_root_.scala.Any"
-    val AnyRef       = t"_root_.scala.AnyRef"
-    val Product      = t"_root_.scala.Product"
-    val ProductImpl  = q"_root_.dataclasses.ProductImpl"
-    val IndexedSeq   = q"_root_.scala.collection.immutable.IndexedSeq"
+    val name    = Term.Name(tname.value)
+    val ctorref = Ctor.Ref.Name(tname.value)
 
-    val params1: sciSeq[Term.Param] = paramss.head
+    val Any           = t"_root_.scala.Any"
+    val AnyRef        = t"_root_.scala.AnyRef"
+    val sciIndexedSeq = q"_root_.scala.collection.immutable.IndexedSeq"
+    val Product       = t"_root_.scala.Product"
+    val ProductImpl   = q"_root_.dataclasses.ProductImpl"
+    val ScalaRunTime  = q"_root_.scala.runtime.ScalaRunTime"
 
-    val newMods = mods withMod Mod.Final()
+    val mods = mods0 withMod Mod.Final()
 
-    val newParamss = (params1 map (_ withMod Mod.ValParam())) +: paramss.tail
+    val params0 = paramss0.head
 
-    val withs = params1 map { p =>
-      val name @ Term.Name(_) = p.name
-      val Some(decltpe) = p.decltpe
+    val params  = params0 map (_ withMod Mod.ValParam())
+    val paramss = params +: paramss0.tail
+
+    val     aexprs = params0 map { case Term.Param(_, name @ Term.Name(_),             _, _) =>   arg"$name"                   }
+    val copyParams = params0 map { case Term.Param(_, name @ Term.Name(_), Some(decltpe), _) => param"$name: $decltpe = $name" }
+    val      withs = params0 map { case Term.Param(_, name @ Term.Name(_), Some(decltpe), _) =>
       val withName = Term.Name("with" + name.value.capitalize)
       q"def $withName($name: $decltpe = $name): $tname = copy($name = $name)"
     }
 
-    val args = params1 map { p =>
-      val name @ Term.Name(_) = p.name
-      arg"$name"
-    }
+    val ctorNew = Term.New(Template(Nil, sciSeq(q"$ctorref(..$aexprs)"), Term.Param(Nil, Name.Anonymous(), None, None), None))
 
-    val copyParams = params1 map { p =>
-      val name @ Term.Name(_) = p.name
-      val Some(decltpe) = p.decltpe
-      param"$name: $decltpe = $name"
-    }
+    val applyParams = params0 map (_ withoutMod Mod.ValParam())
 
-    val applyParams = params1 map { p =>
-      val name @ Term.Name(_) = p.name
-      val Some(decltpe) = p.decltpe
-      param"$name: $decltpe"
-    }
+    val classDefn = q"""
+      ..$mods class $tname[..$tparams] ..$ctorMods (...$paramss) extends { ..$earlyStats } with ..$ctorcalls { $selfParam =>
+        ..$stats;
 
-    val ctorRefName = Ctor.Ref.Name(tnameString)
-    val ctorApply = q"$ctorRefName(..$args)"
-    val ctorNew = Term.New(
-      Template(
-        Nil,
-        sciSeq(ctorApply),
-        Term.Param(Nil, Name.Anonymous(), None, None),
-        None
-      )
-    )
+        ..$withs;
 
-//  val copy = q"def copy(..$copyParams): $tname = new $tname(..$args)"
-    val copy = q"def copy(..$copyParams): $tname = $ctorNew"
+        def copy(..$copyParams): $tname = $ctorNew
 
-    val toString = q"override def toString = $ScalaRunTime._toString(asProduct)"
-    val hashCode = q"override def hashCode = $ScalaRunTime._hashCode(asProduct)"
-    val equals = q"""override def equals(that: $Any) = (this eq that.asInstanceOf[$AnyRef]) || (that match {
-      case that: $tname => $ScalaRunTime._equals(this.asProduct, that.asProduct)
-      case _            => false
-      })"""
+        override def toString = $ScalaRunTime._toString(asProduct)
+        override def hashCode = $ScalaRunTime._hashCode(asProduct)
+        override def equals(that: $Any) = (this eq that.asInstanceOf[$AnyRef]) || (that match {
+          case that: $tname => $ScalaRunTime._equals(this.asProduct, that.asProduct)
+          case _            => false
+        })
 
-    val asProduct = q"private def asProduct: $Product = $ProductImpl($tnameString, $IndexedSeq(..$args))"
+        private def asProduct: $Product = $ProductImpl(${tname.value}, $sciIndexedSeq(..$aexprs))
+      }
+    """
 
-    val apply = q"def apply(..$applyParams): $tname = $ctorNew"
-
-    val newStats = stats ++ withs :+ copy :+ toString :+ hashCode :+ equals :+ asProduct
-
-    val newTemplate = template"{ ..$earlydefns } with ..$ctorcalls { $param => ..$newStats }"
-    val classDefn = q"..$newMods class $tname[..$tparams] ..$ctorMods (...$newParamss) extends $newTemplate"
-
-    val objectDef = q"object ${Term.Name(tnameString)} { $apply }"
+    val objectDef = q"""
+      object $name {
+        def apply(..$applyParams): $tname = $ctorNew
+      }
+    """
 
     Term.Block(sciSeq(classDefn, objectDef))
   }
 }
 
 object `package` {
-  implicit class ModWith_===(private val m: Mod) extends AnyVal {
-    def ===(mod: Mod) = ScalaRunTime._equals(mod, m)
+  implicit class ModWith_===(private val mod: Mod) extends AnyVal {
+    def ===(thatMod: Mod) = ScalaRunTime._equals(mod, thatMod)
   }
   implicit class ModsWithMod(private val mods: sciSeq[Mod]) extends AnyVal {
-    def withMod(mod: Mod) = if (mods exists (mod === _)) mods else mods :+ mod
+    def    withMod(mod: Mod) = if (mods exists (mod === _)) mods else mods :+ mod
+    def withoutMod(mod: Mod) = mods.filterNot(mod === _)
   }
-  implicit class TermParamWithMod(private val p: Term.Param) extends AnyVal {
-    def withMod(mod: Mod) = if (p.mods exists (mod === _)) p else p.copy(mods = p.mods :+ mod)
+  implicit class TermParamWithMod(private val param: Term.Param) extends AnyVal {
+    def    withMod(mod: Mod) = if (param.mods exists (mod === _)) param else param.copy(mods = param.mods :+ mod)
+    def withoutMod(mod: Mod) = param.copy(mods = param.mods.filterNot(mod === _))
   }
 }
