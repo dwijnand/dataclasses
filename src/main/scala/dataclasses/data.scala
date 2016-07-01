f70e69e107a183f7e2610b30c0654c48e069c233
Dale Wijnand <dale.wijnand@gmail.com>
2016-07-01 16:41:15 +0100

    Change toString to have an extra space

--- src/main/scala/dataclasses/data.scala
+++ src/main/scala/dataclasses/data.scala
@@ -5,6 +5,7 @@ import scala.meta._
 import scala.{ collection => sc }, sc.{ immutable => sci }, sci.{ Seq => sciSeq }
 import scala.runtime.ScalaRunTime
 
+// TODO: Add java.io.Serializable?
 class data extends scala.annotation.StaticAnnotation {
   inline def apply(defn: Any) = meta {
     val q"""
@@ -18,27 +19,27 @@ class data extends scala.annotation.StaticAnnotation {
     val AnyRef        =    t"_root_.scala.AnyRef"
     val AnyVal        = ctor"_root_.scala.AnyVal"
     val Boolean       =    t"_root_.scala.Boolean"
-    val sciIndexedSeq =    q"_root_.scala.collection.immutable.IndexedSeq"
     val Product       =    t"_root_.scala.Product"
     val ProductImpl   =    q"_root_.dataclasses.ProductImpl"
     val ScalaRunTime  =    q"_root_.scala.runtime.ScalaRunTime"
+    val scIterator    =    q"_root_.scala.collection.Iterator"
+    val sciIndexedSeq =    q"_root_.scala.collection.immutable.IndexedSeq"
 
     val finalMods = mods withMod Mod.Final()
 
-    val params0 = paramss.head
+    val params = paramss.head
 
-    val valParams  = params0 map (_ withMod Mod.ValParam())
+    val valParams  = params map (_ withMod Mod.ValParam())
     val valParamss = valParams +: paramss.tail
 
-    val     aexprs = params0 map { case Term.Param(_, name @ Term.Name(_),             _, _) =>   arg"$name"                   }
-    val copyParams = params0 map { case Term.Param(_, name @ Term.Name(_), Some(decltpe), _) => param"$name: $decltpe = $name" }
-    val      withs = params0 map { case Term.Param(_, name @ Term.Name(_), Some(decltpe), _) =>
+    val     aexprs = params map { case Term.Param(_, name @ Term.Name(_),             _, _) =>   arg"$name"                   }
+    val copyParams = params map { case Term.Param(_, name @ Term.Name(_), Some(decltpe), _) => param"$name: $decltpe = $name" }
+    val      withs = params map { case Term.Param(_, name @ Term.Name(_), Some(decltpe), _) =>
       val withName = Term.Name("with" + name.value.capitalize)
       q"def $withName($name: $decltpe = $name): $tname = copy($name = $name)"
     }
 
     def anonParam = Term.Param(Nil, Name.Anonymous(), None, None)
-
     val ctorref = Ctor.Ref.Name(tname.value)
     val ctorNew = Term.New(Template(Nil, List(q"$ctorref(..$aexprs)"), anonParam, None))
 
@@ -50,7 +51,7 @@ class data extends scala.annotation.StaticAnnotation {
 
         def copy(..$copyParams): $tname = $ctorNew
 
-        override def toString = $ScalaRunTime._toString(asProduct)
+        override def toString = $scIterator(..$aexprs).mkString(${tname.value} + "(", ", ", ")")
         override def hashCode = $ScalaRunTime._hashCode(asProduct)
         override def equals(that: $Any) = (this eq that.asInstanceOf[$AnyRef]) || (that match {
           case that: $tname => $ScalaRunTime._equals(this.asProduct, that.asProduct)
@@ -62,10 +63,10 @@ class data extends scala.annotation.StaticAnnotation {
     """
 
     val name = Term.Name(tname.value)
-    val nonValParams = params0 map (_ withoutMod Mod.ValParam())
+    val nonValParams = params map (_ withoutMod Mod.ValParam())
 
-    val unapplyAndExtractor = if (params0.size <= 1) Nil else {
-      val defDefns = params0.zipWithIndex map { case (param, idx) =>
+    val unapplyAndExtractor = if (params.size <= 1) Nil else {
+      val defDefns = params.zipWithIndex map { case (param, idx) =>
         val Term.Param(_, name @ Term.Name(_), Some(decltpe @ Type.Name(_)), _) = param
         q"def ${Term.Name(s"_${idx + 1}")}: $decltpe = x.$name"
       }
