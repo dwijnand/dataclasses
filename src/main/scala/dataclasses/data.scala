3b765800c32c84cdb97aadf7bed7be83a41e5c78
Dale Wijnand <dale.wijnand@gmail.com>
2016-07-01 12:36:12 +0100

    Add decltpe to defDefns in Extractor

--- src/main/scala/dataclasses/data.scala
+++ src/main/scala/dataclasses/data.scala
@@ -8,14 +8,11 @@ import scala.runtime.ScalaRunTime
 class data extends scala.annotation.StaticAnnotation {
   inline def apply(defn: Any) = meta {
     val q"""
-      ..$mods0 class $tname[..$tparams] ..$ctorMods (...$paramss0) extends { ..$earlyStats } with ..$ctorcalls {
+      ..$mods class $tname[..$tparams] ..$ctorMods (...$paramss) extends { ..$earlyStats } with ..$ctorcalls {
         $selfParam =>
         ..$stats
       }
     """ = defn
-
-    val name    = Term.Name(tname.value)
-    val ctorref = Ctor.Ref.Name(tname.value)
 
     val Any           = t"_root_.scala.Any"
     val AnyRef        = t"_root_.scala.AnyRef"
@@ -25,12 +22,12 @@ class data extends scala.annotation.StaticAnnotation {
     val ProductImpl   = q"_root_.dataclasses.ProductImpl"
     val ScalaRunTime  = q"_root_.scala.runtime.ScalaRunTime"
 
-    val mods = mods0 withMod Mod.Final()
+    val finalMods = mods withMod Mod.Final()
 
-    val params0 = paramss0.head
+    val params0 = paramss.head
 
-    val params  = params0 map (_ withMod Mod.ValParam())
-    val paramss = params +: paramss0.tail
+    val valParams  = params0 map (_ withMod Mod.ValParam())
+    val valParamss = valParams +: paramss.tail
 
     val     aexprs = params0 map { case Term.Param(_, name @ Term.Name(_),             _, _) =>   arg"$name"                   }
     val copyParams = params0 map { case Term.Param(_, name @ Term.Name(_), Some(decltpe), _) => param"$name: $decltpe = $name" }
@@ -41,31 +38,11 @@ class data extends scala.annotation.StaticAnnotation {
 
     def anonParam = Term.Param(Nil, Name.Anonymous(), None, None)
 
+    val ctorref = Ctor.Ref.Name(tname.value)
     val ctorNew = Term.New(Template(Nil, sciSeq(q"$ctorref(..$aexprs)"), anonParam, None))
 
-    val applyParams = params0 map (_ withoutMod Mod.ValParam())
-
-    val unapplyAndExtractor = if (params0.size <= 1) Nil else { // only 2+ because of https://issues.scala-lang.org/browse/SI-9836
-      val unapply = q"def unapply(x: $tname): Extractor = new Extractor(x)"
-
-      val isEmpty = q"def isEmpty: $Boolean = false"
-      val get = q"def get: Extractor = this"
-      val defs = params0.zipWithIndex map { case (Term.Param(_, name @ Term.Name(_), Some(decltpe), _), idx) =>
-        q"def ${Term.Name(s"_${idx + 1}")} = x.$name"
-      }
-      val extractor = q"""
-        final class Extractor(private val x: $tname) extends _root_.scala.AnyVal {
-          $isEmpty
-          ..$defs
-          $get
-        }
-      """
-
-      sciSeq(unapply, extractor)
-    }
-
     val classDefn = q"""
-      ..$mods class $tname[..$tparams] ..$ctorMods (...$paramss) extends { ..$earlyStats } with ..$ctorcalls { $selfParam =>
+      ..$finalMods class $tname[..$tparams] ..$ctorMods (...$valParamss) extends { ..$earlyStats } with ..$ctorcalls { $selfParam =>
         ..$stats;
 
         ..$withs;
@@ -83,9 +60,29 @@ class data extends scala.annotation.StaticAnnotation {
       }
     """
 
+    val name = Term.Name(tname.value)
+    val nonValParams = params0 map (_ withoutMod Mod.ValParam())
+
+    val unapplyAndExtractor = if (params0.size <= 1) Nil else {
+      val defDefns = params0.zipWithIndex map { case (param, idx) =>
+        val Term.Param(_, name @ Term.Name(_), Some(decltpe @ Type.Name(_)), _) = param
+        q"def ${Term.Name(s"_${idx + 1}")}: $decltpe = x.$name"
+      }
+      sciSeq(
+        q"def unapply(x: $tname): Extractor = new Extractor(x)",
+        q"""
+          final class Extractor(private val x: $tname) extends _root_.scala.AnyVal {
+            ${q"def isEmpty: $Boolean = false"}
+            ..$defDefns
+            ${q"def get: Extractor = this"}
+          }
+        """
+      )
+    }
+
     val objectDefn = q"""
       object $name {
-        def apply(..$applyParams): $tname = $ctorNew
+        def apply(..$nonValParams): $tname = $ctorNew
 
         ..$unapplyAndExtractor
       }
