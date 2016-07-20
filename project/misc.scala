e244272b58a5d5abd90a1b6fcd3ca561d17d9766
Dale Wijnand <dale.wijnand@gmail.com>
2016-07-20 17:55:36 +0100

    Add noSources & tweak append/remove evidences

--- project/misc.scala
+++ project/misc.scala
@@ -20,6 +20,7 @@ object MiscPlugin extends AutoPlugin {
     def scalaPartV = scalaVersion(CrossVersion.partialVersion)
 
     val noDocs    = Def.settings(sources in (Compile, doc) := Nil, publishArtifact in (Compile, packageDoc) := false)
+    val noSources = Def.settings(publishArtifact in (Compile, packageSrc) := false)
     val noPackage = Def.settings(Keys.`package` := file(""), packageBin := file(""), packagedArtifacts := Map())
     val noPublish = Def.settings(
       publishArtifact := false,
@@ -30,26 +31,25 @@ object MiscPlugin extends AutoPlugin {
     )
     val noArtifacts = Def.settings(noPackage, noPublish)
 
-    // Remove with sbt 0.13.12+
-    implicit def appendOption[T]: Append.Sequence[Seq[T], Option[T], Option[T]] =
-      new Append.Sequence[Seq[T], Option[T], Option[T]] {
-        def appendValue(a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a :+ _)
-        def appendValues(a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a :+ _)
-      }
+    implicit def addRemoveOption[T]: AddRemoveOption[T] = new AddRemoveOption[T]
+    class AddRemoveOption[T] extends AnyRef
+        with Append.Sequence[Seq[T], Option[T], Option[T]]
+        with Remove.Value[Seq[T], Option[T]] with Remove.Values[Seq[T], Option[T]]
+    {
+      def appendValue( a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a :+ _)
+      def appendValues(a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a :+ _)
 
-    implicit def removeOption[T]: Remove.Value[Seq[T], Option[T]] with Remove.Values[Seq[T], Option[T]] =
-      new Remove.Value[Seq[T], Option[T]] with Remove.Values[Seq[T], Option[T]] {
-        def removeValue(a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a filterNot _.==)
-        def removeValues(a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a filterNot _.==)
-      }
+      def removeValue( a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a filterNot _.==)
+      def removeValues(a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a filterNot _.==)
+    }
 
     def wordSeq(s: String): immutable.Seq[String] = (s split "\\s+" filterNot (_ == "")).to[immutable.Seq]
 
-    implicit def appendWords: Append.Values[Seq[String], String] = new Append.Values[Seq[String], String] {
+    implicit object AddRemoveWords extends AnyRef
+        with Append.Values[Seq[String], String]
+        with Remove.Values[Seq[String], String]
+    {
       def appendValues(a: Seq[String], b: String): Seq[String] = a ++ wordSeq(b)
-    }
-
-    implicit def removeWords: Remove.Values[Seq[String], String] = new Remove.Values[Seq[String], String] {
       def removeValues(a: Seq[String], b: String): Seq[String] = a filterNot wordSeq(b).contains
     }
 
